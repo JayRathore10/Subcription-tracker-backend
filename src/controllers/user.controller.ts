d99ab7dd065b426be5ff1b9c742815d95b711fbc
Jay <jayrathore88155@gmail.com>
2026-01-06 12:41:41 +0530

    Create getUsers

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import {Request , Response , NextFunction} from "express";
+import { User } from "../models/user.model";
+
+export const getUsers = async(req : Request ,res : Response , next : NextFunction)=>{
+  try{
+    const users  = await User.find();
+
+    if(users.length === 0){
+      const error : any = new Error("No User found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    return res.status(200).json({
+      success : true  , 
+      data : users 
+    })
+
+  }catch(error){
+    next(error);
+  }
+}
